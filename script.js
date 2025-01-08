884eb369a2aa1dfe576b9926df20742b61390307
KamilBeltowski <109316392+KamilBeltowski@users.noreply.github.com>
2025-01-08 09:37:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// Tablica losowych odpowiedzi
+const responses = [
+  "Świetnie!",
+  "Kto gra główną rolę?",
+  "Lubisz filmy Tego reżysera?",
+  "Będę 10 minut wcześniej",
+  "Może kupimy sobie popcorn?",
+  "Ja wolę Colę",
+  "Zaproszę jeszcze Grześka",
+  "Tydzień temu też byłem w kinie na Diunie",
+  "Ja funduję bilety",
+];
+
+// Funkcja dodająca wiadomość Jolanty
+function sendMessage() {
+  const input = document.getElementById("message-input");
+  const chatContent = document.getElementById("chat-content");
+
+  if (input.value.trim() !== "") {
+    const message = document.createElement("div");
+    message.className = "message jolanta";
+    message.innerHTML = `<img src="Jolka.jpg" alt="Jolanta Nowak"><p>${input.value}</p>`;
+    chatContent.appendChild(message);
+    chatContent.scrollTop = chatContent.scrollHeight;
+    input.value = "";
+  }
+}
+
+// Funkcja generująca losową odpowiedź Krzysztofa
+function generateRandomResponse() {
+  const chatContent = document.getElementById("chat-content");
+  const randomIndex = Math.floor(Math.random() * responses.length);
+
+  const message = document.createElement("div");
+  message.className = "message krzysztof";
+  message.innerHTML = `<p>${responses[randomIndex]}</p><img src="Krzysiek.jpg" alt="Krzysztof Łukasiński">`;
+  chatContent.appendChild(message);
+  chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+// Przypisywanie funkcji do przycisków
+document.getElementById("send-button").addEventListener("click", sendMessage);
+document
+  .getElementById("random-response")
+  .addEventListener("click", generateRandomResponse);
